9c8cf523f4d910e5a83efe865410e573f2e5fc1a
MarcoRagnar <marco.huerta.sc@pm.me>
2020-10-02 16:28:33 -0500

    PropTypes

--- src/App.js
+++ src/App.js
@@ -1,71 +1,78 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
-
+import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
 
 function App() {
 
-  //definir el state
-  const [presupuesto,guardarPresupuesto]= useState(0); 
-  const [restante,guardarRestante] = useState(0);
-  const [mostrarpregunta, actualizarPregunta]= useState(true);
-  const [gastos, guardarGastos] = useState([]);
+  // definir el state
+  const [ presupuesto, guardarPresupuesto] = useState(0);
+  const [ restante, guardarRestante] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos] = useState([]);
+  const [ gasto, guardarGasto ] = useState({});
+  const [ creargasto, guardarCrearGasto ] = useState(false);
 
+  // UseEffect que actualiza el restante
 
-  //Cuando generemos un nuevo gasto
-  const agregarNuevoGasto = gasto => {
-    guardarGastos([
-      ...gastos,
-         gasto
-    ])
-  }
+  useEffect(() => {
+      if(creargasto) {
+
+          // agrega el nuevo presupuesto
+          guardarGastos([
+            ...gastos,
+            gasto
+          ]);
+
+          // resta del presupuesto actual
+          const presupuestoRestante = restante - gasto.cantidad;
+          guardarRestante(presupuestoRestante);
+
+          // Resetear a false
+          guardarCrearGasto(false);
+      }
+  }, [gasto, creargasto, gastos, restante]);
 
 
   return (
-  <div className="container">
+    <div className="container">
+        <header>
+            <h1>Gasto Semanal</h1>
 
-  <header>
-    <h1>Gasto Semanal</h1>
+            <div className="contenido-principal contenido">
+              { mostrarpregunta ?  
+                ( 
+                  <Pregunta 
+                    guardarPresupuesto={guardarPresupuesto}
+                    guardarRestante={guardarRestante}
+                    actualizarPregunta={actualizarPregunta}
+                  />
+                )  :  (
+                  <div className="row">
+                      <div className="one-half column">
+                          <Formulario 
+                            guardarGasto={guardarGasto}
+                            guardarCrearGasto={guardarCrearGasto}
+                          />
+                      </div>
 
-    <div className="contenido-principal contenido">
-     
-      {mostrarpregunta ?
-      
-      <Pregunta 
+                      <div className="one-half column">
+                         <Listado
+                            gastos={gastos}
+                         />
 
-        guardarPresupuesto={guardarPresupuesto}
-        guardarRestante={guardarRestante}
-        actualizarPregunta={actualizarPregunta}
-        /> : 
-          
-
-           <div className="row">
-
-            <div className="one-half column">
-              <Formulario 
-                agregarNuevoGasto={agregarNuevoGasto}
-              />
+                         <ControlPresupuesto 
+                            presupuesto={presupuesto}
+                            restante={restante}
+                          />
+                      </div>
+                  </div>
+                ) 
+              }
             </div>
-
-            <div className="one-half column">
-              2
-            </div>
-
-
-          </div>
-
-          
-          }
-
-    
-
-    
-
+        </header>
     </div>
-
-  </header>
-
-  </div>
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,97 +1,87 @@
-import React, {useState} from 'react';
-import Error from './Error';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
+import Error from './Error';
 
+const Formulario = ({guardarGasto, guardarCrearGasto}) => {
 
-const Formulario = ({agregarNuevoGasto}) => {
-      
-    const [nombre, guardarNombre]= useState('');
-    const[cantidad, guardarCantidad]= useState(0);
-    const[error, guardarError]= useState(false);
+    const [nombre, guardarNombre] = useState('');
+    const [cantidad, guardarCantidad] = useState(0);
+    const [ error, guardarError] = useState(false);
 
-    //cuando el usuario agrega un gasto
-
+    
+    // cuando el usuario agrega un gasto
     const agregarGasto = e => {
         e.preventDefault();
 
-
-        //validar
-        if (cantidad < 1 || isNaN( cantidad) || nombre.trim() === '') {
+        // validar
+        if(cantidad < 1 || isNaN( cantidad) || nombre.trim() === '') {
             guardarError(true);
             return;
         }
-
         guardarError(false);
 
-
-        //construir el gasto
-
+        // construir el gasto
         const gasto = {
-
-            nombre,
-            cantidad,
+            nombre, 
+            cantidad, 
             id: shortid.generate()
         }
 
-        //pasar el gasto al componente principal
+        // pasar el gasto al componente principal
+        guardarGasto(gasto);
+        guardarCrearGasto(true);
 
-        agregarNuevoGasto(gasto);
-
-
-        //limpiar el form
+        // resetear el form
         guardarNombre('');
         guardarCantidad(0);
     }
 
-    return (
-
-        <form onSubmit={agregarGasto}>
-             
-
+    return ( 
+        <form
+            onSubmit={agregarGasto}
+        >
             <h2>Agrega tus gastos aquí</h2>
 
-
-            {error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto"
-            
-             />  : null }
-
+            { error ? <Error mensaje="Ambos campos son obligatorios o Presupuesto Incorrecto" /> : null }
 
 
             <div className="campo">
-
                 <label>Nombre Gasto</label>
-                <input
-                type="text" 
-                  className="u-full-width"
-                  placeholder="Ej. Transporte"  
-                  value={nombre}
-                  onChange={ e => guardarNombre(e.target.value)}
+                <input 
+                    type="text"
+                    className="u-full-width"
+                    placeholder="Ej. Transporte"
+                    value={nombre}
+                    onChange={e => guardarNombre(e.target.value)}
                 />
             </div>
-
 
             <div className="campo">
-
                 <label>Cantidad Gasto</label>
-                <input
-                type="number" 
-                  className="u-full-width"
-                  placeholder="Ej. 200" 
-                  value={cantidad}
-                  onChange={e => guardarCantidad( parseInt(e.target.value, 10))} 
+                <input 
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Ej. 300"
+                    value={cantidad}
+                    onChange={e => guardarCantidad( parseInt( e.target.value, 10 ) )}
                 />
             </div>
 
-            <input 
-
+            <input
                 type="submit"
                 className="button-primary u-full-width"
                 value="Agregar Gasto"
             />
+
         </form>
-    );
+
+     );
 }
 
-
-export default Formulario
-
+Formulario.propTypes = {
+    guardarGasto: PropTypes.func.isRequired,
+    guardarCrearGasto: PropTypes.func.isRequired
+}
+ 
+export default Formulario;
